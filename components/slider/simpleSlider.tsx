a819c167ef0c9bd75ed08d5c6845f5e4714486ee
manipapan2 <manipapan2@gmail.com>
2026-06-14 04:12:10 +0330

    Fix: rendering children bug

--- components/slider/simpleSlider.tsx
+++ components/slider/simpleSlider.tsx
@@ -1,16 +1,18 @@
 "use client";
 import useEmblaCarousel from "embla-carousel-react";
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
 
-export default function SimpleSlider(children: any) {
+export default function SimpleSlider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const [emblaRef] = useEmblaCarousel();
 
   return (
     <div className="embla max-w-full overflow-hidden relative">
       <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container max-w-full flex">
-            {children}
-        </div>
+        <div className="embla__container max-w-full flex">{children}</div>
       </div>
     </div>
   );
