a2a31b1abb2172915e6ca9742049e455aa61ab56
manipapan2 <manipapan2@gmail.com>
2026-06-14 04:14:04 +0330

    Change: bg color

--- components/common/card.tsx
+++ components/common/card.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 export default function Card({ name, rate, genre }: CardProps) {
   return (
     <div className="mx-4 cursor-pointer">
-      <div className="aspect-9/14 group rounded-md bg-red-600 w-40">
+      <div className="aspect-9/14 group rounded-md bg-gray-600 w-40">
         <div className="group-hover:bg-black/70 w-full flex items-end p-2 rounded-md h-full bg-black/0 transition-[50ms]">
           <div className="flex items-center opacity-0 translate-y-2 group-hover:translate-y-0 group-hover:opacity-100 transition-[50ms] ">
             <i className="text-yellow-500">
