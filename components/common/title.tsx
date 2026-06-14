973b7f22dacf85bffd297563b8f5af0eeec7b209
manipapan2 <manipapan2@gmail.com>
2026-06-14 04:25:18 +0330

    Add: link when needed

--- components/common/title.tsx
+++ components/common/title.tsx
@@ -1,11 +1,34 @@
+import { ChevronRight } from "lucide-react";
+import Link from "next/link";
+
 interface TItleProps {
-    text: string;
+  text: string;
+  href?: string;
 }
 
-export default function Title({text}: TItleProps) {
-    return (
-        <div className=" grow flex m-4">
-            <h3>{text}</h3>
-        </div>
-    )
+export default function Title({ text, href }: TItleProps) {
+  return (
+    <div className="grow flex m-4 font-bold justify-between">
+      {href ? ( 
+        <Link href={href}>
+          <h3>{text}</h3>
+        </Link>
+      ) : (
+        <h3>{text}</h3>
+      )}
+      <div>
+        {href && (
+          <Link
+            href={href}
+            className="flex font-light text-primary hover:text-primary-disabled"
+          >
+            <span>See all</span>
+            <i>
+              <ChevronRight />
+            </i>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 }
