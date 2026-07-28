19cb66cfeb1790852fdc07b75e914b7c265a32a7
manipapan2 <manipapan2@gmail.com>
2026-07-28 13:47:58 +0330

    refactor: change global css file name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
-import "./globals.css";
+import "./global.css";
 import { cn } from "@/lib/utils";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
