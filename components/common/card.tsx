abef6c4061de1aa5aaa7691124a7784407c41794
manipapan2 <manipapan2@gmail.com>
2026-07-31 23:35:01 +0330

    refactor: add new properties and change some styles

--- components/common/card.tsx
+++ components/common/card.tsx
@@ -1,16 +1,23 @@
 import { Star } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 interface CardProps {
+  imageURL: string;
+  id: string
   name: string;
   rate: string | number;
-  genre: string;
+  genre: string[];
+  year: string;
+  country: string;
 }
 
-export default function Card({ name, rate, genre }: CardProps) {
+export default function Card({ imageURL, id, name, rate, genre, year }: CardProps) {
   return (
-    <div className="mx-4 cursor-pointer">
-      <div className="aspect-9/14 group rounded-md bg-gray-600 w-40">
-        <div className="group-hover:bg-black/70 w-full flex items-end p-2 rounded-md h-full bg-black/0 transition-[50ms]">
+    <Link href={`/movies/${id}`} className="mx-4 cursor-pointer">
+      <div className="aspect-9/14 group rounded-md bg-gray-600 w-40 relative">
+      <Image alt={`${name} poster`} src={imageURL} layout="fill" className="w-full h-full rounded-md"/>
+        <div className="group-hover:bg-black/70 absolute w-full flex items-end p-2 rounded-md h-full bg-black/0 transition-[50ms]">
           <div className="flex items-center opacity-0 translate-y-2 group-hover:translate-y-0 group-hover:opacity-100 transition-[50ms] ">
             <i className="text-yellow-500">
               <Star fill="#f0b100" size={20} />
@@ -20,7 +27,7 @@ export default function Card({ name, rate, genre }: CardProps) {
         </div>
       </div>
 
-      <span className="mt-4 flex">{name}</span>
-    </div>
+      <span className="mt-4 line-clamp-2">{name}</span>
+    </Link>
   );
 }
