38337ecf99cab2a91b7e4cd6323d99828fdb4111
manipapan2 <manipapan2@gmail.com>
2026-07-31 23:34:17 +0330

    feat: create an instance for axios

--- axios_instance/instance.ts
+++ axios_instance/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: process.env.PROJECT_ENV == "development" ? "/" : process.env.API_URL,
+const axiosInstance = axios.create({
+  baseURL: 'http://moviesapi.ir',
 });
 
-export default instance;
+export default axiosInstance;
