087638f830e295ccc0de09b82c6fd5072f2fb36d
manipapan2 <manipapan2@gmail.com>
2026-08-03 13:14:44 +0330

    style: remove unwanted div and center the "see all" text

--- app/components/link-title.tsx
+++ app/components/link-title.tsx
@@ -13,17 +13,15 @@ export default function LinkTitle({ children, href }: LinkTItleProps) {
       <Link href={href}>
           <Title>{children}</Title>
         </Link>
-      <div>
         <Link
             href={href}
-            className="flex font-light text-primary hover:text-primary-disabled"
+            className="flex items-center font-light text-primary hover:text-primary-disabled"
           >
             <span>See all</span>
             <i>
               <ChevronRight />
             </i>
           </Link>
-      </div>
     </div>
   );
 }
