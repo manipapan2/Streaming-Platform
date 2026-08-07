0a0b26bc5f0e44a762bcf94e8625dc81d4f36858
manipapan2 <manipapan2@gmail.com>
2026-08-07 21:56:39 +0330

    style: replace name and position, add truncate style + text-center

--- app/movies/[id]/components/cast.tsx
+++ app/movies/[id]/components/cast.tsx
@@ -8,8 +8,8 @@ export default function Cast({imageUrl, position, name}: CastProps) {
     return (
         <div className="flex flex-col items-center mx-5">
             <div className="w-24 rounded-full aspect-square bg-secondary"></div>
-            <span className="mt-2 opacity-50">{position}</span>
-            <span className="mt-3">{name}</span>
+            <span className="mt-4 truncate max-w-32 text-center">{name}</span>
+            <span className="mt-2 opacity-50 truncate max-w-32 text-center">{position}</span>
         </div>
     )
 }
