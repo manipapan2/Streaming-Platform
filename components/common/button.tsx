7e2907e3dd6c1a72642f36f40f8871a1851de1ab
manipapan2 <manipapan2@gmail.com>
2026-08-13 12:06:02 +0330

    bug: add cn for managing class names

--- components/common/button.tsx
+++ components/common/button.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import React, { ButtonHTMLAttributes, ReactNode } from "react";
 
 type ButtonProps = {
@@ -10,7 +11,11 @@ export default function Button(props: ButtonProps) {
 
   return (
     <button
-      className={`w-30 h-10 bg-primary flex justify-center items-center text-black rounded-md hover:bg-transparent hover:text-foreground border-2 border-primary select-none transition-all ${restProps.disabled ? "pointer-events-none! opacity-30" : ''} ${className}`}
+      className={cn(
+        `h-10 bg-primary p-3 flex justify-center items-center text-black rounded-md hover:bg-transparent hover:text-foreground border-2 border-primary select-none transition-all`,
+        className,
+        restProps.disabled ? "pointer-events-none! opacity-30" : "",
+      )}
       {...restProps}
     >
       {children}
